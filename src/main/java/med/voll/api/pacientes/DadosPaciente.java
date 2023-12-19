414fda80777ecc7350dfdcc25dd8969769bce596
gabrielmota7 <motagabriel819@gmail.com>
2023-12-19 10:13:23 -0300

    Automatizando DTO

--- src/main/java/med/voll/api/pacientes/DadosPaciente.java
+++
@@ -1,7 +0,0 @@
-package med.voll.api.pacientes;
-
-import med.voll.api.endereco.DadosEndereco;
-
-public record DadosPaciente(String nome, String email, String telefone, String cpf, DadosEndereco endereco) {
-
-}
